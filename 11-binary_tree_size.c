b79a63656e74b58b68a432df30d235b22089a20c
Emily Chew <emilychewsh@gmail.com>
2025-08-07 04:48:56 +0000

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ * Return: size of bt, 0 if tree is null
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	/* size = size of left subtree + size of right subtree + 1 (current) */
+	size = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
+
+	return (size);
+}
